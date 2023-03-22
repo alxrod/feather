7c74528dde6f6f4236c90dbbe9a5d085fd4289fc
alxrod <alexbrodriguez@gmail.com>
2023-03-22 01:48:11 +0000

    fixing font linking

--- ui/featherapp/pages/_app.js
+++ ui/featherapp/pages/_app.js
@@ -4,6 +4,7 @@ import store from '../store'
 import { Provider } from 'react-redux'
 import '../styles/globals.css'
 import AppWrapper from "../components/app_wrapper";
+import Head from 'next/head';
 
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
@@ -12,14 +13,35 @@ const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
 const FeatherApp = ({Component, pageProps}) => {
   return (
-    <Provider store={store}>
-      <AppWrapper>
-        <Elements stripe={stripePromise}>
-          <Navbar/>
-          <Component {...pageProps}/>
-        </Elements>
-      </AppWrapper>
-    </Provider>
+    <>
+      <Head>
+      <meta charset="utf-8" />
+      <link rel="icon" href={process.env.NEXT_PUBLIC_FRONTEND_URL+"/favicon.ico"} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta
+        name="description"
+        content="Web site created using create-react-app"
+      />
+      <link rel="apple-touch-icon" href={process.env.NEXT_PUBLIC_FRONTEND_URL+"/logo192.png"} />
+      
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet"/>
+      
+      <link rel="manifest" href={process.env.NEXT_PUBLIC_FRONTEND_URL+"/manifest.json"} />
+
+      <title>Feather</title>
+      </Head>
+      <Provider store={store}>
+        <AppWrapper>
+          <Elements stripe={stripePromise}>
+            <Navbar/>
+            <Component {...pageProps}/>
+          </Elements>
+        </AppWrapper>
+      </Provider>
+    </>
   )
 }
 
